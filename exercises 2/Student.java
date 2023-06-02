7bc817022b8dd7fcdad03e4c3d7dd23a520ae373
jamesduong03 <135298206+jamesduong03@users.noreply.github.com>
2023-06-02 16:07:03 +1200

    Add files via upload

---
+++ exercises 2/Student.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class Student 
+{
+	String firstname;
+	String lastname;
+	String studentID;
+	
+	Student(String firstname, String lastname, String studentID)
+	{
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.studentID = studentID;
+	}
+	
+	Student(String firstname, String lastname)
+	{	
+		this(firstname, lastname, "?");
+	}
+		
+	Student()
+	{
+		this("UNKNOWN", "UNKNOWN", "?");
+	}
+	
+	public static void main(String[] args) 
+	{
+		Scanner keyboard = new Scanner (System.in);
+	
+		System.out.println("Please enter the student's first name:");
+		String firstname = keyboard.nextLine();
+		
+		
+		System.out.println("Please enter the student's last name:");
+		String lastname = keyboard.nextLine();
+		
+		System.out.println("Please enter the student's ID:");
+		String studentID = keyboard.nextLine();
+		
+		
+		System.out.println("Calling 3 parameter constructor:");
+		Student student = new Student(firstname, lastname, studentID);
+		System.out.println("Student's fisrt name: " + student.firstname + " Last name: " + student.lastname + " ID: " + student.studentID);
+		
+		
+		System.out.println("Calling 2 parameter constructor:");
+		student = new Student(firstname, lastname);
+		System.out.println("Student's fisrt name: " + student.firstname + " Last name: " + student.lastname + " ID: " + student.studentID);
+
+		
+		System.out.println("Calling zero parameter constructor:");
+		student = new Student();
+		System.out.println("Student's fisrt name: " + student.firstname + " Last name: " + student.lastname + " ID: " + student.studentID);
+
+	}
+}
